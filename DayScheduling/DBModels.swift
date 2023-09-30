9b367dd22940f4d2ae622c99514258debfb77032
Tsuri Yuma <yuma33t@gmail.com>
2023-09-30 23:39:13 +0900

    ハッカソン1日目

---
+++ DayScheduling/DBModels.swift
@@ -0,0 +1,8 @@
+//
+//  DBModels.swift
+//  DayScheduling
+//
+//  Created by 釣悠馬 on 2023/09/30.
+//
+
+import Foundation
---
+++ DayScheduling/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  DayScheduling
+//
+//  Created by 釣悠馬 on 2023/09/30.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
