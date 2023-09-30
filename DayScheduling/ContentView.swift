77c8a2dcf96beb782ed7375983e4be1ef4189d3a
Tsuri Yuma <yuma33t@gmail.com>
2023-09-30 23:15:19 +0900

    Initial Commit

---
+++ DayScheduling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayScheduling
+//
+//  Created by 釣悠馬 on 2023/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayScheduling/DaySchedulingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaySchedulingApp.swift
+//  DayScheduling
+//
+//  Created by 釣悠馬 on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct DaySchedulingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
