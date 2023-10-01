ca3e71f0bec287e1f8c0184168fb5ceffc21c580
Tsuri Yuma <yuma33t@gmail.com>
2023-10-01 14:04:31 +0900

    カレンダーの追加

---
+++ DayScheduling/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  DayScheduling
+//
+//  Created by 釣悠馬 on 2023/10/01.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
